24eda37cf605bf525065e1af16ee17976ec52cf4
bhashas <b.hashas@laposte.net>
2025-12-09 00:56:01 +0100

    Fix backend: add AWS profile terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    profile        = "terraform"
     bucket         = "terraform-state-brahim-hashas"
     key            = "lab/terraform.tfstate"
     region         = "eu-west-3"
