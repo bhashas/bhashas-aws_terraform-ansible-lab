87afad4f95ebf922271b319662c26db6ca4bbf78
bhashas <b.hashas@laposte.net>
2025-12-09 00:59:57 +0100

    Format Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@
 
 provider "aws" {
   region  = var.aws_region
-  profile = "terraform"  # profil que tu utilises déjà localement
+  profile = "terraform" # profil que tu utilises déjà localement
 }
 
 #######################################
